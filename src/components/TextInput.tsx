d051fcb66de53e5668c1d2ac72292e05cc266049
wnishimura <uyamuya4153@gmail.com>
2024-05-04 11:41:11 +0900

    vitest設定

---
+++ src/components/TextInput.tsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+
+const TextInput = () => {
+  const [text, setText] = useState("");
+  return (
+    <>
+      <div>
+        <input
+          type="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          aria-label="Text Input"
+        />
+      </div>
+      <p>Entered Text: {text}</p>
+    </>
+  );
+};
+
+export default TextInput;
