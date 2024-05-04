6a39e256aec9d7c190a98e4323a843bacb1c1872
wnishimura <uyamuya4153@gmail.com>
2024-05-04 12:32:38 +0900

    eslint設定

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
 const TextInput = () => {
-  const [text, setText] = useState("");
+  const [text, setText] = useState('');
   return (
     <>
       <div>
